06611c5e00855f137758afab88af0a5ac048d09d
faridLim <gjsl1457@gmail.com>
2021-09-20 23:36:08 +0900

    Initial Commit

---
+++ QuoteGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuoteGenerator
+//
+//  Created by 임재헌 on 2021/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
